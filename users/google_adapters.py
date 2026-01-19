50b433a30afd241bf12f1134fe38bb3d473fc495
mbharath246 <bharathroyal246@gmail.com>
2026-01-19 22:53:54 +0530

    bug fix for existing users google login

--- users/google_adapters.py
+++ users/google_adapters.py
@@ -1,8 +1,24 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-
+from budget.models import Users
 
 
 class GoogleAccountAdapter(DefaultSocialAccountAdapter):
+    
+    def pre_social_login(self, request, sociallogin):
+        """
+        Link Google account to existing user with same email
+        """
+        if sociallogin.is_existing:
+            return
+
+        email = sociallogin.account.extra_data.get("email")
+        
+        try:
+            user = Users.objects.get(email=email)
+            sociallogin.connect(request, user)
+            
+        except Users.DoesNotExist:
+            pass
     
     def save_user(self, request, sociallogin, form=None):
         user = super().save_user(request, sociallogin, form)
